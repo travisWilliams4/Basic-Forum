12b718f0a5ad44058d83adf92897c71ce45933ec
Travis Williams <travis.w.williams4@gmail.com>
2018-06-26 00:09:37 -0500

    Add files via upload

---
+++ create_cat.php
@@ -0,0 +1,37 @@
+<?php
+//create_cat.php
+require 'DatabaseConnection.php';
+include 'header.php';
+
+if($_SERVER['REQUEST_METHOD'] != 'POST')
+{
+    //the form hasn't been posted yet, display it
+    // "required" under category name forces them to fill it out
+    echo '<form method="post" action="">
+        Category name: <input type="text" name="cat_name" required/><br>
+        Category description:<br> <textarea name="cat_description" /></textarea><br>
+        <input type="submit" value="Add category" />
+     </form>';
+}
+else
+{   
+    $sql = "INSERT INTO 
+                categories(cat_name, cat_description)
+            VALUES('" . mysqli_real_escape_string($conn, $_POST['cat_name']) . "',
+                   '" . mysqli_real_escape_string($conn, $_POST['cat_description']) . "')";
+               
+            
+    $result = mysqli_query($conn, $sql);
+    if(!$result)
+    {
+        //something went wrong, display the error
+        echo 'Error: ' . mysqli_error($conn);
+    }
+    else
+    {
+        echo 'New category successfully added.';
+    }
+
+}
+?>
+
---
+++ databaseConnection.php
@@ -0,0 +1,11 @@
+<?php
+/// info to connect to the database
+$server = 'localhost';
+$username = 'root';
+$password = 'neky8012';
+$database = 'helloworldsql';
+
+/// creates a mysqli connection
+$conn = new mysqli($server, $username, $password, $database)
+
+?>
